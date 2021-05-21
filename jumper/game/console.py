29a2726f0f605e4a96204ff2912d7665f74895c9
NickCampbell91 <nickcampbell1991@gmail.com>
2021-05-21 07:59:04 -0600

    Update console.py

--- jumper/game/console.py
+++ jumper/game/console.py
@@ -41,3 +41,12 @@ class Console:
             text (string): The text to display.
         """
         print(text)
+        
+   def picture(self, text_art):
+        """Displays the given text art on the screen. 
+
+        Args: 
+            self (Screen): An instance of Screen.
+            text_art (list): The text art to display.
+        """
+        print(*text_art, sep = "\n")
